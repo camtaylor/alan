82397dee2fced339402f64ee341e9b9ae7fc04a7
jmelberg <jordan.melberg@gmail.com>
2016-03-25 17:24:25 -0700

    Revert "added osx support for web browsing"
    This reverts commit 6fb124b6708c0ccdc793ea93ddb2129f2b02a339.

--- actions/go.py
+++ actions/go.py
@@ -1,11 +1,5 @@
 import webbrowser
-import sys
-import os
 
 def open_url(url):
   print url
-  if sys.platform == "darwin":
-    command = "open http://" + url  
-    os.system(command)
-  else:
-    webbrowser.open(url)
+  webbrowser.open(url)
