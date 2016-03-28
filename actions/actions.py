4d40f56aeb2591231992a637a17c494c4090d932
Cameron Taylor <camerontaylor00@gmail.com>
2016-03-27 17:30:37 -0700

    reformatted actions.py and started memory

--- actions/actions.py
+++ actions/actions.py
@@ -23,9 +23,13 @@ from language import jokes as joke
       
 """
 
+from memory import short_term
+memory = short_term.Memory()
+
 # Action functions go here
 def open_url(sentence):
   """
+   Dispatch: go
    Function to open a web browser at a specific url.
   """
   urls = [word[0] for word in sentence if ".com" in word[0]]
@@ -38,9 +42,11 @@ def open_url(sentence):
       webbrowser.open(url)
     return "Navigating to " + url
 
+
 def display_picture(sentence):
   """
-    Function to send a message.
+    Dispatch: show
+    Function to display a picture.
   """
   from language import wikipage
   concept = " ".join([word[0] for word in sentence if "N" in word[1]])
@@ -52,17 +58,12 @@ def display_picture(sentence):
   else:
     return "Sorry but I can't find a picture of that." 
 
-# Picks an action function from the actions_dictionary. Shouldn't need to be altered to add a new function.
-def pick_action(verb, sentence):
-  global actions_dictionary
-  verb = verb.lower()
-  if verb in actions_dictionary.keys():
-    return actions_dictionary[verb](sentence)
-  else:
-    return "I don't have an action for the verb " + verb
-
 
 def manual(sentence):
+  """
+    Dispatch: help
+    Function to list out the possible actions.
+  """
   action_list = """
   1. Go to a website - Say 'go to '
   2. Show an Image - Say 'show me '
@@ -71,10 +72,20 @@ def manual(sentence):
   """
   return "Here is what I can do:" + action_list
 
+
 def tell_a_joke(sentence):
+  """
+    Dispatch: tell
+    Function to tell a joke.
+  """
   return joke.chuck_norris_jokes(sentence)
 
+
 def wolfram_alpha(sentence):
+  """
+    Dispatch: find
+    Function to calculate math equations.
+  """
   import wolframalpha
   app_id ='WX22RG-YHK38JAEWA'
   client = wolframalpha.Client(app_id)
@@ -88,6 +99,37 @@ def wolfram_alpha(sentence):
   return "Could not find anything"
 
 
+def remember(sentence):
+  """
+    Dispatch: remember
+    Function to remember something in short term. Key value storage dict.
+  """
+  # TODO store a real memory instead of dummy
+  global memory
+  concept_key = " ".join([word[0] for word in sentence if 'N' in word[1]])
+  memory.remember_concept(concept_key, "Dummy memory about " + concept_key)
+  return "I will remember that " + concept_key
+
+
+def recall(sentence):
+  """
+    Dispatch: recall
+    Function to recall something from memory.
+  """
+  # Check for command recall all
+  if len(sentence) == 2 and sentence[1][0] == "all":
+    return memory.recall_all()
+  concept_key = " ".join([word[0] for word in sentence if 'N' in word[1]])
+  concept = memory.recall_concept(concept_key)
+  if concept:
+    return concept
+  else:
+    return "I don't have a short term memory for key " + concept_key
+
+
+# TODO write an action to forget short term memory
+
+
 # This dictionary is used as a dispatcher. The verb is the key and the function that is called is the value.
 actions_dictionary = {
 
@@ -96,7 +138,21 @@ actions_dictionary = {
   "help": manual,
   "tell": tell_a_joke,
   "find": wolfram_alpha,
-  # Should be calculate, but voice has a hard time picking it up
+  "remember": remember,
+  "recall": recall,
 }
 
+
+def pick_action(verb, sentence):
+  """
+    Function to choose an action based on the dispatch verb.
+    Only allows for one action per verb as of now will add
+    multiple action functionality in the future.
+  """
+  global actions_dictionary
+  verb = verb.lower()
+  if verb in actions_dictionary.keys():
+    return actions_dictionary[verb](sentence)
+  else:
+    return "I don't have an action for the verb " + verb
 
--- memory/short_term.py
+++ memory/short_term.py
@@ -1,8 +1,25 @@
 
 
 class Memory(object):
-  
+  """
+    This class is to be used as a global short term memory for Alan.
+    Should only be based on a session in computer's memory and not
+    persistent onto disk.
+  """
+
+  concept_dict = {}
 
   def __init__(self):
     concept_stack = []
     concept_dict = {}
+
+  def remember_concept(self, concept_key, concept_value):
+    self.concept_dict[concept_key] = concept_value
+
+  def recall_concept(self, concept_key):
+    if concept_key in self.concept_dict.keys():
+      return self.concept_dict[concept_key]
+
+  def recall_all(self):
+    if len(self.concept_dict.keys()) > 0:
+      return "I have short term memories for " + " and ".join(self.concept_dict.keys())
