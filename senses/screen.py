6721f6c61293780b11afe328aae47739e28f5619
camtaylor <camerontaylor00@gmail.com>
2016-05-18 03:03:51 -0700

    Added mac support to the screenshot file

--- senses/screen.py
+++ senses/screen.py
@@ -1,14 +1,34 @@
-import gtk.gdk
+
+"""
+  This is a simple script to capture the current screen and save to an image file.
+  While there are no hooks into alan as a whole yet, eventually this will be very
+  useful.
+
+  When alan starts using machine learning algorithms the screenshot can be used to
+  verify state. This state will allow alan to understand if the correct task has
+  been done or not.
+
+"""
+
+import sys
+import os
 
 
 if __name__ == "__main__":
-  w = gtk.gdk.get_default_root_window()
-  sz = w.get_size()
-  print "The size of the window is %d x %d" % sz
-  pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, sz[0], sz[1])
-  pb = pb.get_from_drawable(w, w.get_colormap(), 0, 0, 0, 0, sz[0], sz[1])
-  if (pb != None):
-    pb.save("screenshot.png", "png")
-    print "Screenshot saved to screenshot.png."
+
+  # If it's a mac, (AKA "darwin") gtk won't work. So run the command
+  if sys.platform == "darwin":
+    os.system("screencapture screenshot.png")
+  # Otherwise in any other system capture via python.
   else:
-    print "Unable to get the screenshot."
+    import gtk.gdk
+    w = gtk.gdk.get_default_root_window()
+    sz = w.get_size()
+    print "The size of the window is %d x %d" % sz
+    pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, sz[0], sz[1])
+    pb = pb.get_from_drawable(w, w.get_colormap(), 0, 0, 0, 0, sz[0], sz[1])
+    if (pb != None):
+      pb.save("screenshot.png", "png")
+      print "Screenshot saved to screenshot.png."
+    else:
+      print "Unable to get the screenshot."
